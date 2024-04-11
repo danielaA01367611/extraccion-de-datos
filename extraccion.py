db5e41b980cc935d6c48363f9e5a86a3ef4517b3
danielaA01367611 <A01367611@tec.mx>
2024-04-10 18:15:48 -0600

    filtro1

--- extraccion.py
+++ extraccion.py
@@ -3,3 +3,7 @@ import pandas as pd
 print("Aplicacion de extraccion de datos")
 df=pd.read_excel("detalle_precios_productos_fabricados_2022.xlsx")
 print(df.info)
+df.head()
+
+filtro1=df[df["NOMBRE_VENDEDOR"] == "LETICIA RAMIREZ HERNANDEZ"]
+filtro1
