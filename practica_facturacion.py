6bbc0fbcc50e0904e1de97b04a27f746d5a89f7c
danielaA01367611 <A01367611@tec.mx>
2024-04-10 18:35:28 -0600

    filtro 2

--- practica_facturacion.py
+++ practica_facturacion.py
@@ -1,8 +1,12 @@
 import pandas as pd
 
 df=pd.read_excel("datos_facturacion.xlsx")
-print(df.head())
+#print(df.head())
 
 filtro1=df[(df["CVE_CLPV"] >= 1000) & (df["CVE_CLPV"]<=2000)]
-print(filtro1)
+#print(filtro1)
 filtro1.to_csv('practica_facturacion1.csv')
+
+filtro2 = df[~(df["CVE_VEND"] == 5.0 ) & ~(df["CVE_VEND"] == 4.0)]
+print(filtro2)
+filtro2.to_csv('practica_facturacion2.csv')
