776a4d7d994e0e18b753584b38df8f2f28fea696
danielaA01367611 <A01367611@tec.mx>
2024-04-10 19:09:57 -0600

    filtro 3

--- extraccion.py
+++ extraccion.py
@@ -9,3 +9,13 @@ df=pd.read_excel("detalle_precios_productos_fabricados_2022.xlsx")
 filtro1=df[df["NOMBRE_VENDEDOR"] == "LETICIA RAMIREZ HERNANDEZ"]
 #print(filtro1)
 filtro1.to_csv('entregable1.csv')
+
+#por filas
+filtro2= df.iloc[2:8,: ]   
+#print(filtro2)
+filtro2.to_csv('entregable2.csv')
+
+#Filtro por columnas
+filtro3=df.iloc[ : , [1, 3, 4,10]]  
+print(filtro3)
+filtro3.to_csv('entregable3.csv')
