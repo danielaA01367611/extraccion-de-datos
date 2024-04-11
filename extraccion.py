cf00157d959e09c38f76aa889028f745d14d1cd3
danielaA01367611 <A01367611@tec.mx>
2024-04-10 19:20:59 -0600

    filtro 3

--- extraccion.py
+++ extraccion.py
@@ -17,5 +17,13 @@ filtro2.to_csv('entregable2.csv')
 
 #Filtro por columnas
 filtro3=df.iloc[ : , [1, 3, 4,10]]  
-print(filtro3)
+#print(filtro3)
 filtro3.to_csv('entregable3.csv')
+
+df1= pd.read_excel('detalle_precios_productos_fabricados_2022.xlsx', index_col=1)
+#print(df1)
+
+#Filtro 4: Se filtro un filtro de filas con columnas. Mostrando únicamente las filas que tenga la fecha de doc (que es el índice) igual a 2022-11-22 y 2022-12-23. Mostrando la columna de subtotal_partida.
+filtro4=df1.loc[["2022-11-22","'2022-12-23"], ["SUBTOTAL_PARTIDA"]]
+print(filtro4)
+filtro4.to_csv('entregable4.csv')
